ebe8351b9d64be2775b87a9947fcdcb33569180c
Boris Starkov <boris.starkow@gmail.com>
2021-03-30 22:22:22 +0300

    add basic menu using antd framework

--- app/src/frontend-react/App.js
+++ app/src/frontend-react/App.js
@@ -1,47 +1,67 @@
-import React from 'react'
+import 'antd/dist/antd.css';
+
+import React from 'react';
+
+import {Layout, Menu} from 'antd'
+
+
+import MainPage from "./MainPage";
+import StatisticsPage from "./StatisticsPage";
+
+
+const {Header, Content} = Layout;
+
+
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      testFolder: undefined
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: 'main'
+        }
     }
-  }
 
-  async componentDidMount() {
-      let tf = await window.backend.readLocal('tests-folder')
-      if (!tf) {
-          tf = (await window.backend.selectFolder()).filePaths
-          await window.backend.writeLocal('tests-folder', tf)
-      }
-      console.log(`tf is ${tf}`)
-      this.setState({
-          testFolder: tf
-      })
-  }
 
-  async reselectFolder () {
-      let {filePaths} = await window.backend.selectFolder()
-      await window.backend.writeLocal('tests-folder', filePaths)
-      this.setState({
-          testFolder: filePaths
-      })
-  }
+    handleClick = (e) => {
+        console.log('click', e)
+        this.setState({
+            current: e.key
+        })
+    }
 
-  render() {
-    return (
-        <div className="App">
-          <div className="TestsFolderAddress">
-            {this.state.testFolder}
-          </div>
-          <button onClick={() => this.reselectFolder()}>
-              Reselect folder
-          </button>
-        </div>
-    )
-  }
+    render() {
+        let content;
 
+        if (this.state.current === 'main') {
+            content = <MainPage />
+        } else {
+            content = <StatisticsPage />
+        }
+
+        return (
+            <Layout>
+                <Header style={{background: '#FFF'}}>
+                    <Menu
+                        onClick={this.handleClick}
+                        defaultSelectedKeys={['main']}
+                        selectedKeys={this.state.current}
+                        mode='horizontal'
+                        theme='light'>
+                            <Menu.Item key='main' style={{fontSize: '20px', fontWeight: 'bold'}}>
+                                Главная
+                            </Menu.Item>
+                            <Menu.Item key='stats' style={{fontSize: '20px', fontWeight: 'bold'}}>
+                                Пациенты
+                            </Menu.Item>
+                    </Menu>
+                </Header>
+                <Content>
+                    {content}
+                </Content>
+            </Layout>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ app/src/frontend-react/StatisticsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class StatisticsPage extends React.Component {
+    render() {
+        return (
+            <div>
+                This is statistics Page.
+            </div>
+        )
+    }
+}
+
+export default StatisticsPage;
