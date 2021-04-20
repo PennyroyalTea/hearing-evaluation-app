6d6c2c0bf369f2e2dfa13134046feb1e0b261886
Boris Starkov <boris.starkow@gmail.com>
2021-04-20 18:09:07 +0300

    add version to settings page

--- app/src/frontend-react/SettingsPage.js
+++ app/src/frontend-react/SettingsPage.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import {Typography, Button, Divider} from "antd";
 
-const {Text} = Typography;
+const {Text, Title} = Typography;
 
 class SettingsPage extends React.Component {
+    version = '1.0.0'
+
     render() {
         return (
-            <div>
+            <>
                 <Text type={this.props.tfolder.status === 'ok' ? 'success' : 'danger'}>
                     {this.props.tfolder.status === 'ok'
                         ? this.props.tfolder.path : 'Папка с тестами не найдена :('}
@@ -16,7 +18,11 @@ class SettingsPage extends React.Component {
                 <Button onClick={this.props.tfolderReloader}>
                     загрузить папку с тестами
                 </Button>
-            </div>
+                <Divider />
+                <Title level={5} align={'center'}>
+                    Версия {this.version}
+                </Title>
+            </>
         )
     }
 }
